0eed1e75e010fd35afb6df4cf6217bc4fdeea92a
Nanami7-7 <597458410@qq.com>
2025-03-17 14:45:48 +0800

    Add files via upload

---
+++ qg_homework/LinkList/Sources/初始化单链表.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
+#include<stdio.h>
+struct LinkNode* Init_LinkList()
+{   //创建头结点
+	struct LinkNode* head = malloc(sizeof(struct LinkNode));
+	head->data = -1;
+	head->next = NULL;
+	int val;
+	//创建尾指针
+	struct LinkNode* pRear = head;
+	while (1)
+	{
+		
+		printf("请输入数据,输入-1则结束\n");
+		scanf("%d", &val);
+		if (val == -1)
+		{
+			break;
+		}
+		
+		
+		//创建新节点
+		struct LinkNode* NewNode = malloc(sizeof(struct LinkNode));
+		NewNode->data = val;
+		NewNode->next = NULL;
+		//插入新节点
+		pRear->next = NewNode;
+		//更新尾指针
+		pRear = NewNode;
+		
+	}
+	return head;
+}
